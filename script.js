ef974d833c9b631200808f152ee8672d85c6c41b
Syahril Baso <99664845+syahrilbaso@users.noreply.github.com>
2024-03-10 08:19:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hourEl = document.getElementById('hour');
 const minuteEl = document.getElementById('minute');
 const secondEl = document.getElementById('second');
 
-const Ramadan = new Date('March 11, 2024 00:00:00').getTime();
+const Ramadan = new Date('March 2, 2024 00:00:00').getTime();
 
 updateCountdown()
 
