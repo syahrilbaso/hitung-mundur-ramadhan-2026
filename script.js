be2b08f81e2a6fc9299e10edc160656c6f552d0f
Fireflies Developers <99664845+syahrilbaso@users.noreply.github.com>
2025-01-13 04:03:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hourEl = document.getElementById('hour');
 const minuteEl = document.getElementById('minute');
 const secondEl = document.getElementById('second');
 
-const Ramadan = new Date('April 10, 2024 00:00:00').getTime();
+const Ramadan = new Date('Februari 28, 2025 00:00:00').getTime();
 
 updateCountdown()
 
@@ -12,9 +12,9 @@ function updateCountdown(){
     const gap = Ramadan - now;
 
     const second = 1000;
-    const minute = second * 5000;
-    const hour = minute * 5000;
-    const day = hour * 5000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
     const d = Math.floor(gap/day);
     const h = Math.floor((gap % day) / hour);
