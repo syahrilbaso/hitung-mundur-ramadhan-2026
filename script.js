1db2e63f334b8fe1e43ecf1158a83029e576de5e
Fireflies Developers <99664845+syahrilbaso@users.noreply.github.com>
2025-01-13 03:52:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function updateCountdown(){
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
-    const day = hour * 66;
+    const day = hour * 100;
 
     const d = Math.floor(gap/day);
     const h = Math.floor((gap % day) / hour);
